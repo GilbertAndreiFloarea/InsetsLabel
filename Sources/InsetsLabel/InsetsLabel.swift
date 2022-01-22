0012d9d41ac77f1199622d378d6f1c1a10bcfc6c
Gilbert Andrei Floarea <gilbertandrei@gmail.com>
2022-01-22 20:49:57 +0200

    InsetsLabel

---
+++ Sources/InsetsLabel/InsetsLabel.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+@IBDesignable class InsetsLabel: UILabel {
+
+    @IBInspectable var topInset: CGFloat = 5.0
+    @IBInspectable var bottomInset: CGFloat = 5.0
+    @IBInspectable var leadingInset: CGFloat = 7.0
+    @IBInspectable var trailingInset: CGFloat = 7.0
+
+    override func drawText(in rect: CGRect) {
+        let insets = UIEdgeInsets(
+            top: topInset,
+            left: leadingInset,
+            bottom: bottomInset,
+            right: trailingInset
+        )
+        super.drawText(in: rect.inset(by: insets))
+    }
+
+    override var intrinsicContentSize: CGSize {
+        let size = super.intrinsicContentSize
+        return CGSize(
+            width: size.width + leadingInset + trailingInset,
+            height: size.height + topInset + bottomInset
+        )
+    }
+
+    override var bounds: CGRect {
+        didSet {
+            preferredMaxLayoutWidth = bounds.width - leadingInset - trailingInset
+        }
+    }
+}
